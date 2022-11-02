9b03ede56b3005938a7bb3f4b59eeafd76fc2e97
AndyCChen <acchen@cpp.edu>
2022-11-02 14:32:03 -0700

    save work

--- project_1/matrix_generator.cpp
+++ project_1/matrix_generator.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
+void createMatrix(int rows, int cols, int **matrix) {
+   for (int row = 0; row < rows; row++) {
+      for (int col = 0; col < cols; col++) {
+         matrix[row][col] = rand() % 10;
+         cout << matrix[row][col] << " ";
+      }
+      cout << endl;
+   }
+}
+
 int main() {
    srand((unsigned) time(NULL));
-
-   int cols, rows;
+   
+   int cols, rows, numOfMatrices;
 
    cout << "Enter # of cols: ";
    cin >> cols;
@@ -15,8 +27,23 @@ int main() {
    cout << "Enter # of rows: ";
    cin >> rows;
 
-   cout << "Cols: " << cols << endl;
-   cout << "Rows: " << rows << endl;
+   cout << "Enter # of matrices to generate: ";
+   cin >> numOfMatrices;
+
+   int **matrix = new int*[rows];
+
+   for (int row = 0; row < cols; row++) {
+      matrix[row] = new int[cols];
+   }
+
+   for(int i = 0; i < numOfMatrices; i++) {
+      createMatrix(rows, cols, matrix);
+      cout << endl;
+   }
+
+   string path = "/2x2/file.txt";
+   ofstream file(path);
+   file << "stuff";
 
    return 0;
 }

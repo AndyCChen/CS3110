a1e8e998020a90bb810939ff37747ed7bd2a917c
AndyCChen <acchen@cpp.edu>
2022-11-08 15:20:03 -0800

    added matrix data sets

--- project_1/matrix_generator.cpp
+++ project_1/matrix_generator.cpp
@@ -6,13 +6,22 @@
 
 using namespace std;
 
-void createMatrix(int rows, int cols, int **matrix) {
-   for (int row = 0; row < rows; row++) {
-      for (int col = 0; col < cols; col++) {
-         matrix[row][col] = rand() % 10;
-         cout << matrix[row][col] << " ";
+void createMatrices(int rows, int cols, int **matrix, int numberOfMatrices, string folderName) {
+
+   for(int matrixNum = 0; matrixNum < numberOfMatrices; matrixNum++) {
+      string fileName = to_string(matrixNum);
+      string path = folderName + "/" + fileName;
+      ofstream file(path);
+
+      for (int row = 0; row < rows; row++) {
+         for (int col = 0; col < cols; col++) {
+            matrix[row][col] = rand() % 10;
+            file << matrix[row][col] << " ";
+         }
+         file << endl;
       }
-      cout << endl;
+
+      file.close();
    }
 }
 
@@ -20,6 +29,7 @@ int main() {
    srand((unsigned) time(NULL));
    
    int cols, rows, numOfMatrices;
+   string folderName;
 
    cout << "Enter # of cols: ";
    cin >> cols;
@@ -30,20 +40,16 @@ int main() {
    cout << "Enter # of matrices to generate: ";
    cin >> numOfMatrices;
 
+   cout << "Enter name of folder: ";
+   cin >> folderName;
+
    int **matrix = new int*[rows];
 
    for (int row = 0; row < cols; row++) {
       matrix[row] = new int[cols];
    }
+   createMatrices(rows, cols, matrix, numOfMatrices, folderName);
 
-   for(int i = 0; i < numOfMatrices; i++) {
-      createMatrix(rows, cols, matrix);
-      cout << endl;
-   }
-
-   string path = "/2x2/file.txt";
-   ofstream file(path);
-   file << "stuff";
-
+   cout << endl;
    return 0;
 }

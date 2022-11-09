2fa2bb7c0756080a8c9d7d1fdc702a23f45bbc56
AndyCChen <acchen@cpp.edu>
2022-11-08 16:34:06 -0800

    save work

--- project_1/classical.cpp
+++ project_1/classical.cpp
@@ -1,45 +1,42 @@
 #include <iostream>
 #include <string>
-#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
 
 using namespace std;
 
-void getMatrixValues(int **matrixA, int ** matrixB, int maxRows, int maxCols, int matrixNum, string folderName) { 
-   string fileName = to_string(matrixNum);
-   string path = folderName + "/" + fileName;
-
-   ifstream file(path);
-
+void getMatrixValues(int **matrixA, int ** matrixB, int maxRows, int maxCols, int matrixNum) { 
    for (int row = 0; row < maxRows; row++) {
       for (int col = 0; col < maxCols; col++) {
-         int value;
-         file >> value;
-
-         matrixA[row][col] = value;
-         matrixB[row][col] = value;
+         matrixA[row][col] = rand() % 10;
+         matrixB[row][col] = rand() % 10;
       }
    }
 }
 
-void multiplyMatrices() {
-
+void multiplyMatrices(int **matrixA, int ** matrixB, int maxRows, int maxCols) {
+   for (int row = 0; row < maxRows; row++) {
+      for (int col = 0; col < maxCols; col++) {
+         cout << matrixA[row][col] << " ";
+      }
+      cout << endl;
+   }
 }
 
 int main() {
-   int cols, rows, numOfMatrices;
-   string folderName;
+   srand((unsigned) time(NULL));
+
+   const int NUM_OF_MATRICES = 1000;
+   const int RUNS_PER_SET = 20;
+
+   int cols, rows;
 
    cout << "Enter # of cols: ";
    cin >> cols;
    
    cout << "Enter # of rows: ";
    cin >> rows;
-
-   cout << "Enter # of matrices to read: ";
-   cin >> numOfMatrices;
-
-   cout << "Enter name of folder: ";
-   cin >> folderName;
 
    int **matrixA = new int*[rows];
    int **matrixB = new int*[rows];
@@ -49,16 +46,20 @@ int main() {
       matrixB[row] = new int[cols];
    }
 
-   for (int matrixNumber = 0; matrixNumber < numOfMatrices; matrixNumber++) {
-      getMatrixValues(matrixA, matrixB, rows, cols, matrixNumber, folderName);
+   for (int matrixNumber = 0; matrixNumber < NUM_OF_MATRICES; matrixNumber++) {
+      getMatrixValues(matrixA, matrixB, rows, cols, matrixNumber);
 
-      for (int row = 0; row < rows; row++) {
-         for (int col = 0; col < cols; col++) {
-            cout << matrixA[row][col];
-         }
-         cout << endl;
+      for (int runNumber = 0; runNumber < RUNS_PER_SET; runNumber++) {
+
+         auto start = chrono::high_resolution_clock::now();
+
+         multiplyMatrices(matrixA, matrixB, rows, cols);
+
+         auto stop = chrono::high_resolution_clock::now();
+
+         duration<double, chrono::milliseconds> ms_double = stop - start;
+         cout << ms_double.count() << endl;
       }
-      cout << endl;
    }
 
    return 0;
